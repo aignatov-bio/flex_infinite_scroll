97afb3a6d0f2f60101abd465e3f853c7eeef9303
Anton Ignatov <belmek@me.com>
2020-03-08 23:17:25 +0100

    Refactor JS library

--- vendor/assets/javascript/flex_infinite_scroll.js
+++ vendor/assets/javascript/flex_infinite_scroll.js
@@ -1,5 +1,92 @@
 // Flex Infinite Scroll initialization
 
+class FlexIS {
+    constructor(objectName, config = {}) {
+        this.targetObject = document.querySelector(objectName)
+        this.config = config
+        this.loading = false
+        this.currentPage = this.config.startPage || 0
+        this.nextPage = this.currentPage + 1
+        
+        this.config.loadMargin = this.config.loadMargin || 150
+        this.config.eventTarget = objectName == 'body' ? window : this.targetObject
+    }
+    
+    init() {
+        this.#getData();
+        this.config.eventTarget.addEventListener('scroll', () => {
+        	var scrollTop= this.config.eventTarget.scrollTop || this.config.eventTarget.scrollY;
+        	var containerSize = this.config.eventTarget.innerHeight || this.targetObject.offsetHeight;
+        	var scrollSize = this.#scrollHeight();
+        	if (scrollTop + containerSize > scrollSize - this.config.loadMargin && this.nextPage !== 'last') {
+                this.#getData()
+        	};
+        })
+    }
+    
+    resetScroll() {
+        this.currentPage = this.config.startPage || 0
+        this.nextPage = this.currentPage + 1
+        this.#getData();
+    }
+    
+    // Private methods 
+    
+    
+    #getData = (page = this.currentPage ) => {
+        // Check for loading process and last page
+        console.log(this.#scrollNotApear())
+        if (this.loading || page === 'last') return false;
+        this.loading = true;
+        
+        var xhr = new XMLHttpRequest();
+        var params = {page: this.nextPage};
+        xhr.open('GET', this.config.url + '?' + this.#urlParams(params));
+        xhr.onload = () => {
+          var json = JSON.parse(xhr.response)
+          if (xhr.status === 200) {
+            var div = document.createElement('div');
+            div.innerHTML = json.data;
+            while (div.children.length > 0) {
+                this.targetObject.appendChild(div.children[0]);
+            }
+            this.nextPage = json.next_page || 'last';
+            this.loading = false;
+            if (this.#scrollNotApear()) {
+              // check if on initial load not enough elements on screen
+              this.#getData();
+            }
+          } else {
+            this.loading = false;
+          }
+        };
+        xhr.send();
+    }
+    
+    #scrollHeight = () => {
+        return this.targetObject.scrollHeight
+    }
+    
+    #urlParams = (params) => {
+        var encodedString = [];
+        for (var prop in params) {
+            if (params.hasOwnProperty(prop)) {
+                encodedString.push(encodeURI(prop + '=' + params[prop]));
+            }
+        }
+        return encodedString.join('&');
+    }
+    
+    #scrollNotApear = () => {
+      var containerSize = this.config.eventTarget.innerHeight || this.targetObject.offsetHeight;
+      var scrollSize = this.#scrollHeight();
+      console.log(scrollSize - containerSize - this.config.loadMargin)
+      return (scrollSize - containerSize - this.config.loadMargin <= 0);
+    }
+} 
+
+
+
 
 function flexInfiniteScroll(object_id,config = {}) {
     // Config preparing
